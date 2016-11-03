544342041ac4c66ae1036b57b64109a0ccd4d95b
Grebiel José Ifill Brito <wh38esoq@rbg.informatik.tu-darmstadt.de>
2016-11-03 10:48:08 +0100

    tong project rx refactoring

--- refactored-examples/vehk-footong/src/main/java/de/tong/util/FontManager.java
+++ refactored-examples/vehk-footong/src/main/java/de/tong/util/FontManager.java
@@ -1,35 +1,30 @@
 package de.tong.util;
 
-import java.awt.Font;
+import rx.Observable;
+import rx.schedulers.Schedulers;
+
+import javax.swing.*;
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
-
-import javax.swing.SwingWorker;
 
 public class FontManager {
 
     public static Map<String, Font> fontMap = new HashMap<String, Font>();
-    private static SwingWorker<Map<String, Font>, Void> worker;
 
     public static void loadFonts() {
-	worker = new SwingWorker<Map<String, Font>, Void>() {
 
-	    @Override
-	    protected void done() {
-		try {
-		    fontMap = get();
-		    System.out.println("Fonts loaded.");
+		// RxRefactoring
+		Observable
+				.fromCallable(() -> doInBackground()) // fromCallable will always return one emission
+				.subscribeOn(Schedulers.computation())
+				.observeOn(Schedulers.immediate())
+				.doOnNext(asyncResult -> done(asyncResult)) // use onNext instead of onCompleted, because the first emission is already the result from doInBackground
+				.subscribe();
+    }
 
-		} catch (InterruptedException e) {
-		    e.printStackTrace();
-		} catch (ExecutionException e) {
-		    e.printStackTrace();
-		}
-	    }
-
-	    @Override
-	    protected Map<String, Font> doInBackground() throws Exception {
+    // RxRefactoring: changed to static, because it is called from a static method
+	private static Map<String, Font> doInBackground() throws Exception {
 		System.out.println("Loading fonts...");
 		Map<String, Font> fonts = new HashMap<String, Font>();
 
@@ -39,11 +34,14 @@ public class FontManager {
 		fonts.put("pointsFont", new Font("Consolas", Font.BOLD, 30));
 
 		return fonts;
-	    }
+	}
 
-	};
-	worker.run();
-    }
+	// RxRefactoring: parameter added due to the "get()" method, try catch block removed (needed because of get())
+	// RxRefactoring: changed to static, because it is called from a static method
+	private static void done(Map<String, Font> result) {
+			fontMap = result;
+			System.out.println("Fonts loaded.");
+	}
 
     public static Font getFont(String key) {
 	return fontMap.get(key);
