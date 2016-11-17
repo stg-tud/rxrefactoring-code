a0b892801711636c575b5b8dab76602c76a6462a
Grebiel José Ifill Brito <wh38esoq@rbg.informatik.tu-darmstadt.de>
2016-11-17 17:15:41 +0100

    expected files added to git

---
+++ RxRefactoringTests/resources/swingworker.anonymous.classes/ExpectedClassCase6.java
@@ -0,0 +1,64 @@
+package rxrefactoring;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+import rx.Observable;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
+public class AnonymousClassCase6
+{
+	public void start()
+	{
+		Observable
+				.fromCallable( new Callable<String>()
+				{
+					@Override
+					public String call() throws Exception
+					{
+						longRunningOperation();
+						return "DONE";
+					}
+				} ).subscribeOn( Schedulers.computation() )
+				.observeOn( Schedulers.immediate() )
+				.doOnNext( new Action1<String>()
+				{
+					@Override
+					public void call( String asyncResult )
+					{
+						try
+						{
+							String result = asyncResult;
+							String anotherGet = AnonymousClassCase6.this.get();
+							System.out.println( "[Thread: " + Thread.currentThread().getName() + "] Result:" + result);
+						}
+						catch ( Exception e )
+						{
+							e.printStackTrace();
+						}
+					}
+				} )
+				.timeout( 3L, TimeUnit.SECONDS )
+				.onErrorReturn( new Func1<Throwable, String>()
+				{
+					@Override
+					public String call( Throwable throwable )
+					{
+						return null;
+					}
+				} )
+				.subscribe();
+	}
+
+	private String get()
+	{
+		return "some string";
+	}
+
+	private void longRunningOperation() throws InterruptedException
+	{
+		Thread.sleep( 4000L );
+		System.out.println( "[Thread: " + Thread.currentThread().getName() + "] Long running operation completed." );
+	}
+}
