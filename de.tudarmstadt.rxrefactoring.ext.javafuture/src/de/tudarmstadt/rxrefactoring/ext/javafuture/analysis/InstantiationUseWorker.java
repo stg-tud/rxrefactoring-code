10c3713e39aae89b03330655609765435970145a
camgbus <camilag.bustillo@gmail.com>
2018-04-16 18:18:38 +0200

    Revert "Started precondition analysis"
    This reverts commit 36da45e089127fab1f2b207a1dc32adbb7e33402.

--- de.tudarmstadt.rxrefactoring.ext.javafuture/src/de/tudarmstadt/rxrefactoring/ext/javafuture/analysis/InstantiationUseWorker.java
+++ de.tudarmstadt.rxrefactoring.ext.javafuture/src/de/tudarmstadt/rxrefactoring/ext/javafuture/analysis/InstantiationUseWorker.java
@@ -1,38 +1,24 @@
 package de.tudarmstadt.rxrefactoring.ext.javafuture.analysis;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.Assignment;
-import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.Expression;
-import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
-import org.eclipse.jdt.core.dom.Name;
 import org.eclipse.jdt.core.dom.ReturnStatement;
-import org.eclipse.jdt.core.dom.SimpleName;
 
 import com.google.common.collect.HashMultimap;
-import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multimap;
-import com.google.common.collect.Multiset;
 
 import de.tudarmstadt.rxrefactoring.core.IProjectUnits;
-import de.tudarmstadt.rxrefactoring.core.IRewriteCompilationUnit;
 import de.tudarmstadt.rxrefactoring.core.IWorker;
 import de.tudarmstadt.rxrefactoring.core.RefactorSummary.WorkerSummary;
 import de.tudarmstadt.rxrefactoring.core.analysis.impl.reachingdefinitions.UseDef;
 import de.tudarmstadt.rxrefactoring.core.analysis.impl.reachingdefinitions.UseDef.Use;
-import de.tudarmstadt.rxrefactoring.core.analysis.impl.reachingdefinitions.UseDef.Use.Kind;
-import de.tudarmstadt.rxrefactoring.core.utils.ASTNodes;
 import de.tudarmstadt.rxrefactoring.ext.javafuture.instantiation.SubclassInstantiationCollector;
 
 /**
@@ -43,24 +29,14 @@ import de.tudarmstadt.rxrefactoring.ext.javafuture.instantiation.SubclassInstant
 public class InstantiationUseWorker implements IWorker<SubclassInstantiationCollector, InstantiationUseWorker> {
 
 	// Maps MethodInvocations and ClassInstanceCreations that return Future or
-	// Future subclass instantiations to the latter instance uses.
-	Multimap<ASTNode, Use> instantiationUses = HashMultimap.create();
-	
-	// Stores ClassInstanceCreations and MethodInvocations of instances onto which
-	// only supported methods are called.
-	public Multiset<ASTNode> toRefactorInstantiations = HashMultiset.create();
-	
-	// Maps the names attributed to instance creations for which the refactoring
-	// is supported to the MethodDeclaration where they are declared.
-	public Multimap<String, MethodDeclaration> instantiationNames = HashMultimap.create();
-	
+	// Future subclass
+	// instantiations to the latter instance uses.
+	Multimap<ASTNode, Use> classInstantiationsToUses = HashMultimap.create();
 	// Maps MethodInvocations that return Future or Future subclass collections to
 	// the latter instance uses.
 	Multimap<MethodInvocation, Use> collectionCreationsToUses = HashMultimap.create();
-	
+
 	Map<ASTNode, UseDef> analysis;
-	
-	List<String> notSupported = Arrays.asList("cancel", "isCancelled", "isDone");
 
 	@Override
 	public @Nullable InstantiationUseWorker refactor(@NonNull IProjectUnits units,
@@ -76,42 +52,25 @@ public class InstantiationUseWorker implements IWorker<SubclassInstantiationColl
 		collectionCreations.addAll(input.methodInvReturnCollection.values());
 		collectionCreations.addAll(input.methodInvReturnSubclassCollection.values());
 
-		analysis.values().forEach(useDef -> {
-			Set<Expression> definitions = useDef.asMap().keySet();
-			definitions.forEach(expr -> {
-				Set<Use> exprUses = useDef.getUses(expr);
-				if (instantiations.contains(expr)) {
-					exprUses.stream()
-						.forEach(use -> instantiationUses.put(expr, use));
-				}
-				if (collectionCreations.contains(expr)) {
-					exprUses.stream()
-						.forEach(use -> collectionCreationsToUses.put((MethodInvocation) expr, use));
-				}
-			});
-		});
-		
-		toRefactorInstantiations.addAll(instantiationUses.keySet());
-		// Callee of not supported method
-		instantiationUses.forEach((node, use) -> {
-			if (use.getOp() instanceof MethodInvocation) {
-				MethodInvocation methodInv = (MethodInvocation) use.getOp();
-				if (notSupported.contains(methodInv.getName().toString()))
-					toRefactorInstantiations.remove(node);
-			}
-			});
-		
-		// Collect identifiers of supported instances
-		instantiationUses.forEach((expr, use) -> {
-			if (toRefactorInstantiations.contains(expr) && use.getName()!=null) {
-				Optional<MethodDeclaration> methodDecl= ASTNodes.findParent(expr, MethodDeclaration.class);
-				if (methodDecl.isPresent())
-					instantiationNames.put(use.getName().toString(), methodDecl.get());
+		analysis.forEach((node, useDef) -> {
+			Set<Expression> defs = useDef.asMap().keySet();
+
+			// Stores Uses of kinds METHOD_INVOCATION, METHOD_PARAMETER and RETURN
+			if (node instanceof MethodInvocation || node instanceof ReturnStatement) {
+				defs.forEach(expr -> {
+					Set<Use> exprUses = useDef.getUses(expr);
+					if (instantiations.contains(expr)) {
+						exprUses.stream().filter(use -> use.getOp().equals(node))
+								.forEach(use -> classInstantiationsToUses.put(expr, use));
+					}
+					if (collectionCreations.contains(expr)) {
+						exprUses.stream().filter(use -> use.getOp().equals(node))
+								.forEach(use -> collectionCreationsToUses.put((MethodInvocation) expr, use));
+					}
+
+				});
 			}
 		});
-		
-		//TODO Part of assignment of not supported expression
-		
 		return this;
 	}
 
