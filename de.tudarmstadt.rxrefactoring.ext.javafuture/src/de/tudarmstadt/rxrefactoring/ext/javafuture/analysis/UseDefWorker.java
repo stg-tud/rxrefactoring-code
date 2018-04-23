ed36cdfd03f4ed5d920494c769de37cb97219df5
Mirko Köhler <kohler.mirko@gmail.com>
2018-04-23 15:00:08 +0200

    UseDef analysis now also includes subclasses

--- de.tudarmstadt.rxrefactoring.ext.javafuture/src/de/tudarmstadt/rxrefactoring/ext/javafuture/analysis/UseDefWorker.java
+++ de.tudarmstadt.rxrefactoring.ext.javafuture/src/de/tudarmstadt/rxrefactoring/ext/javafuture/analysis/UseDefWorker.java
@@ -1,15 +1,26 @@
 package de.tudarmstadt.rxrefactoring.ext.javafuture.analysis;
 
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
+import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.BodyDeclaration;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.Initializer;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
 import com.google.common.collect.Maps;
 
 import de.tudarmstadt.rxrefactoring.core.IProjectUnits;
+import de.tudarmstadt.rxrefactoring.core.IRewriteCompilationUnit;
 import de.tudarmstadt.rxrefactoring.core.IWorker;
 import de.tudarmstadt.rxrefactoring.core.RefactorSummary.WorkerSummary;
 import de.tudarmstadt.rxrefactoring.core.UnitASTVisitor;
@@ -20,6 +31,7 @@ import de.tudarmstadt.rxrefactoring.core.analysis.impl.reachingdefinitions.UseDe
 import de.tudarmstadt.rxrefactoring.core.analysis.impl.reachingdefinitions.UseDefAnalysis;
 import de.tudarmstadt.rxrefactoring.core.utils.Log;
 
+
 /**
  * Adds analysis results to the AST.
  * 
@@ -29,7 +41,6 @@ import de.tudarmstadt.rxrefactoring.core.utils.Log;
 public class UseDefWorker implements IWorker<Void, Map<ASTNode, UseDef>> {
 
 	private static DataFlowAnalysis<ASTNode, UseDef> analysis = UseDefAnalysis.create();
-	// DataFlowAnalysis.create(null, null);
 
 	@Override
 	public @Nullable Map<ASTNode, UseDef> refactor(@NonNull IProjectUnits units, @Nullable Void input,
@@ -37,27 +48,108 @@ public class UseDefWorker implements IWorker<Void, Map<ASTNode, UseDef>> {
 
 		final Map<ASTNode, UseDef> result = Maps.newHashMap();
 
-		units.accept(new UnitASTVisitor() {
-			public boolean visit(MethodDeclaration node) {
-			//	Log.info(getClass(), "method: " + node.resolveBinding().getName() + " --- " + node.resolveBinding().getDeclaringClass());
-				
-				try {
-					result.putAll(analysis.apply(ProgramGraph.createFrom(node.getBody()), analysis.mapExecutor()));
-				} catch (NotConvergingException e) {
-					// TODO Handle case when the dataflow analysis does not yield a correct result.
-					result.putAll((Map<ASTNode, UseDef>) e.getUnfinishedOutput());
-					Log.error(getClass(), "The dataflow analysis did not converge");
-					e.printStackTrace();
-				}
-				
-				return false;
-			}
-		});
-
-		//result.forEach((node, use) -> Log.info(getClass(), "Node: " + node + "\n" + "Use: " + use));
-
+		for (IRewriteCompilationUnit unit : units) {
+			CompilationUnitAnalysis cua = new CompilationUnitAnalysis(unit, result);
+			cua.analyze();			
+		}		
 		
+		//result.forEach((node, use) -> Log.info(getClass(), "Node: " + node + "\n" + "Use: " + use));	
 		return result;
+	}
+	
+	class CompilationUnitAnalysis {
+		
+		private final IRewriteCompilationUnit unit;
+		private final Map<ASTNode, UseDef> result;
+		
+		public CompilationUnitAnalysis(IRewriteCompilationUnit unit, Map<ASTNode, UseDef> result) {
+			this.unit = unit;
+			this.result = result;
+		}
+		
+		public void analyze() {
+			unit.findPrimaryTypeDeclaration().ifPresent(this::analyzeTypeDeclaration);	
+		}
+		
+		
+		private void analyzeBodyDeclarations(List<BodyDeclaration> bodyDeclarations) {
+			
+			for (Object o : bodyDeclarations) {
+				if (o instanceof TypeDeclaration) {
+					analyzeTypeDeclaration((TypeDeclaration) o);
+				} else if (o instanceof MethodDeclaration) {
+					analyzeMethodDeclaration((MethodDeclaration) o);
+				} else if (o instanceof FieldDeclaration) {
+					analyzeFieldDeclaration((FieldDeclaration) o);
+				} else if (o instanceof Initializer) {
+					analyzeInitializer((Initializer) o);
+				} else {
+					//Enum and Annotation declarations not included
+					
+				}
+			}			
+		}
+		
+		
+		private void analyzeTypeDeclaration(TypeDeclaration type) {
+			analyzeBodyDeclarations(type.bodyDeclarations());
+		}
+		
+		
+		private void analyzeMethodDeclaration(MethodDeclaration m) {
+			Block body = m.getBody();
+			
+			if (body == null) {
+				return;
+			}
+			
+			analyzeBlock(body);			
+		}
+		
+		
+		private void analyzeFieldDeclaration(FieldDeclaration f) {
+			//Do something with fields
+		}
+		
+		private void analyzeInitializer(Initializer i) {
+			analyzeBlock(i.getBody());
+		}
+		
+		private void analyzeVariableFragments(List<VariableDeclarationFragment> fragments) {
+			//Do something with variable declarations
+		}		
+		
+				
+		private void analyzeBlock(Block block) {
+			AST ast = unit.getAST();
+			
+			Map<ASTNode, UseDef> tempResult;
+			
+			try {
+				tempResult = analysis.apply(ProgramGraph.createFrom(block), analysis.mapExecutor());				
+			} catch (NotConvergingException e) {
+				// TODO Handle case when the dataflow analysis does not yield a correct result.
+				tempResult = (Map<ASTNode, UseDef>) e.getUnfinishedOutput();
+				Log.error(getClass(), "The dataflow analysis did not converge. Continue with unfinished result.");
+				e.printStackTrace();
+			}
+			
+			result.putAll(tempResult);
+			
+			//Analyze subclasses that are part of a block
+			block.accept(new ASTVisitor() {								
+				@Override
+				public boolean visit(TypeDeclaration type) {
+					analyzeTypeDeclaration(type);
+					return false;				
+				}				
+				@Override
+				public boolean visit(AnonymousClassDeclaration decl) {
+					analyzeBodyDeclarations(decl.bodyDeclarations());
+					return false;
+				}
+			});
+		}
 	}
 
 }
