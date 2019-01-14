bea81d36bda1f943475ffc08c9b86fe484ac5a7d
Nikolas Hanstein <nikolas.hanstein@gmail.com>
2019-01-14 15:54:17 +0100

    Implemented rudimentary calling method detection

--- de.tudarmstadt.rxrefactoring.core/src/de/tudarmstadt/rxrefactoring/core/internal/execution/RandoopGenerator.java
+++ de.tudarmstadt.rxrefactoring.core/src/de/tudarmstadt/rxrefactoring/core/internal/execution/RandoopGenerator.java
@@ -13,13 +13,14 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.PosixFilePermission;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
@@ -28,7 +29,14 @@ import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ChildListPropertyDescriptor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.IVariableBinding;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.MethodReference;
+import org.eclipse.jdt.core.dom.ParameterizedType;
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 import org.eclipse.jdt.core.dom.StructuralPropertyDescriptor;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
@@ -92,9 +100,8 @@ public class RandoopGenerator
             localPath = pkgs[0].getResource().getFullPath().removeFirstSegments(1).toOSString();
         }
 
-        List<String> methodsToOmit = new ArrayList<>();
-        Set<String> classesToTest = new HashSet<>();
-
+        // Find all methods that will be refactored
+        Set<String> changingMethods = new HashSet<>();
         for(IRewriteCompilationUnit unit : units)
         {
             if(unit.hasChanges())
@@ -105,24 +112,43 @@ public class RandoopGenerator
                     TypeDeclaration type = (TypeDeclaration)objType;
                     for(MethodDeclaration method : type.getMethods())
                     {
-                        String className = cu.getPackage().getName() + "." + type.getName().getFullyQualifiedName();
-                        classesToTest.add(className);
-                        Log.info(JavaVisitor.class, "Looking for changes in method " + className + "." + method.getName().getFullyQualifiedName() + "()");
+                        String className = type.resolveBinding().getBinaryName();
+                        String methodName = method.getName().getFullyQualifiedName();
+                        Log.info(JavaVisitor.class, "Looking for changes in method " + className + "." + methodName + "()");
                         List<ASTNode> nodes = new JavaVisitor(node -> nodeHasChanges(node, unit)).visitMethodDeclaration(method);
 
                         if(!nodes.isEmpty())
                         {
-                            methodsToOmit.add(className.replace(".", "\\.") + "\\." + method.getName().getFullyQualifiedName().replace(".", "\\.") + "\\(");
+                            @SuppressWarnings("unchecked") List<SingleVariableDeclaration> objParams = method.parameters();
+                            String params = objParams.stream().map(SingleVariableDeclaration::resolveBinding)
+                                    .map(IVariableBinding::getType)
+                                    .map(ITypeBinding::getBinaryName)
+                                    .collect(Collectors.joining(", "));
+                            changingMethods.add(buildSignature(className, methodName, params, method.getReturnType2().resolveBinding().getBinaryName()));
                         }
                     }
                 }
             }
         }
 
-        createOutput(methodsToOmit, classesToTest, projectPath, localPath);
+        // Find all methods that call methods in changingMethods
+        Set<String> callingMethods = new HashSet<>();
+        JavaVisitor visitor = new JavaVisitor(node -> changingMethods.contains(buildSignatureFromNode(node)));
+        for(IRewriteCompilationUnit unit : units)
+        {
+            CompilationUnit cu = (CompilationUnit)unit.getRoot();
+            List<ASTNode> nodes = visitor.visitCompilationUnit(cu);
+            callingMethods.addAll(nodes.stream()
+                                       .map(RandoopGenerator::buildSignatureFromNode)
+                                       .collect(Collectors.toList()));
+        }
+
+        // TODO Execute the refactoring now
+        // TODO Once the refactoring is done, hook and check if we can still
+        // find methods with signatures matching those in callingMethods
     }
 
-    private static void createOutput(List<String> methodsToOmit, Set<String> classesToTest, String projectPath, String localPath)
+    private static void createOutput(Set<String> methodsToOmit, Set<String> classesToTest, String projectPath, String localPath)
     {
         String time = Calendar.getInstance().getTime().toString().replace(' ', '_').replace(':', '_');
         File randoopTemp = new File(determineTempPath() + File.separator + "randoop-gen-" + time);
@@ -356,6 +382,35 @@ public class RandoopGenerator
             }
         }
         return false;
+    }
+
+    private static String buildSignatureFromNode(ASTNode node)
+    {
+        IMethodBinding binding;
+        if(node instanceof MethodInvocation)
+        {
+            MethodInvocation invocation = (MethodInvocation)node;
+            binding = invocation.resolveMethodBinding().getMethodDeclaration();
+        }
+        else if(node instanceof MethodReference)
+        {
+            MethodReference reference = (MethodReference)node;
+            binding = reference.resolveMethodBinding().getMethodDeclaration();
+        }
+        else
+        {
+            return null;
+        }
+        String className = binding.getDeclaringClass().getBinaryName();
+        String methodName = binding.getName();
+        String params = Arrays.stream(binding.getParameterTypes()).map(t -> t.getBinaryName()).collect(Collectors.joining(", "));
+        String returnName = binding.getReturnType().getBinaryName();
+        return buildSignature(className, methodName, params, returnName);
+    }
+
+    private static String buildSignature(String className, String methodName, String params, String returnName)
+    {
+        return className + "." + methodName + "(" + params + ") -> " + returnName;
     }
 
     private static class CopyVisitor extends SimpleFileVisitor<Path>
