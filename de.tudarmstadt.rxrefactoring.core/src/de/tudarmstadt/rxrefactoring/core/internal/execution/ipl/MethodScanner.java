dcdb737b2b701f54426f7aa95ead2c7e89b2ff5c
Nikolas Hanstein <nikolas.hanstein@gmail.com>
2019-01-21 17:47:36 +0100

    MethodScanner: Added documentation

--- de.tudarmstadt.rxrefactoring.core/src/de/tudarmstadt/rxrefactoring/core/internal/execution/ipl/MethodScanner.java
+++ de.tudarmstadt.rxrefactoring.core/src/de/tudarmstadt/rxrefactoring/core/internal/execution/ipl/MethodScanner.java
@@ -28,16 +28,35 @@ import de.tudarmstadt.rxrefactoring.core.internal.execution.ipl.collect.Immutabl
 import de.tudarmstadt.rxrefactoring.core.internal.execution.ipl.collect.Pair;
 import de.tudarmstadt.rxrefactoring.core.utils.Log;
 
+/**
+ * The method scanner finds methods that are directly impacted by the
+ * refactoring, as well as ones that call the impacted methods.
+ * @author Nikolas Hanstein
+ */
 public class MethodScanner
 {
+    /**
+     * Find both {@link #findImpactedMethods(ProjectUnits) impacted} and
+     * {@link #findCallingMethods(ProjectUnits, Set) calling} methods, i.e.
+     * methods that are directly impacted by the refactoring and methods that
+     * call the impacted ones.
+     * @param units The project compilation units to work on.
+     * @return A pair of impacted methods and calling methods.
+     */
     public static Pair<Set<String>, Set<String>> findMethods(ProjectUnits units)
     {
-        Set<String> changing = findChangingMethods(units);
-        Set<String> calling = findCallingMethods(units, changing);
-        return new ImmutablePair<>(changing, calling);
+        Set<String> impacted = findImpactedMethods(units);
+        Set<String> calling = findCallingMethods(units, impacted);
+        return new ImmutablePair<>(impacted, calling);
     }
 
-    private static Set<String> findChangingMethods(ProjectUnits units)
+    /**
+     * Finds methods that are directly impacted by the refactoring, i.e. ones
+     * that the rewriter flags as 'needing a rewrite'.
+     * @param units The project compilation units to work on.
+     * @return A set of all impacted methods.
+     */
+    private static Set<String> findImpactedMethods(ProjectUnits units)
     {
         Set<String> ret = new HashSet<>();
         for(IRewriteCompilationUnit unit : units)
@@ -50,11 +69,13 @@ public class MethodScanner
                     TypeDeclaration type = (TypeDeclaration)objType;
                     for(MethodDeclaration method : type.getMethods())
                     {
+                        // Store the class and method name, for logging and building signatures
                         String className = type.resolveBinding().getBinaryName();
                         String methodName = method.getName().getFullyQualifiedName();
                         Log.info(JavaVisitor.class, "Looking for changes in method " + className + "." + methodName + "()");
-                        List<ASTNode> nodes = new JavaVisitor(node -> nodeHasChanges(node, unit)).visitMethodDeclaration(method);
 
+                        // Find all nodes that have changes, and build signatures for them
+                        List<ASTNode> nodes = new JavaVisitor(node -> nodeHasChanges(node, unit)).visitMethodDeclaration(method);
                         if(!nodes.isEmpty())
                         {
                             @SuppressWarnings("unchecked") List<SingleVariableDeclaration> objParams = method.parameters();
@@ -71,10 +92,17 @@ public class MethodScanner
         return ret;
     }
 
-    private static Set<String> findCallingMethods(ProjectUnits units, Set<String> changingMethods)
+    /**
+     * Finds methods that directly call impacted methods.
+     * @param units The project compilation units to work on.
+     * @param impactedMethods The methods that have been impacted by the
+     *        refactoring.
+     * @return A set of all calling methods.
+     */
+    private static Set<String> findCallingMethods(ProjectUnits units, Set<String> impactedMethods)
     {
         Set<String> ret = new HashSet<>();
-        JavaVisitor visitor = new JavaVisitor(node -> changingMethods.contains(buildSignatureFromNode(node)));
+        JavaVisitor visitor = new JavaVisitor(node -> impactedMethods.contains(buildSignatureFromNode(node)));
         for(IRewriteCompilationUnit unit : units)
         {
             CompilationUnit cu = (CompilationUnit)unit.getRoot();
@@ -86,6 +114,15 @@ public class MethodScanner
         return ret;
     }
 
+    /**
+     * Builds a method signature for the specified AST node. Note that this only
+     * works if the node is of type {@link MethodInvocation} or
+     * {@link MethodReference}. See
+     * {@link #buildSignature(String, String, String, String)} for the format of
+     * these signatures.
+     * @param node The node to build a signature for.
+     * @return A string representing the built signature.
+     */
     private static String buildSignatureFromNode(ASTNode node)
     {
         IMethodBinding binding;
@@ -103,6 +140,7 @@ public class MethodScanner
         {
             return null;
         }
+
         String className = binding.getDeclaringClass().getBinaryName();
         String methodName = binding.getName();
         String params = Arrays.stream(binding.getParameterTypes()).map(t -> t.getBinaryName()).collect(Collectors.joining(", "));
@@ -110,12 +148,37 @@ public class MethodScanner
         return buildSignature(className, methodName, params, returnName);
     }
 
+    /**
+     * Builds a signature based on the specified method properties. These
+     * signatures are of the form
+     * {@code my.package.MyClass.myMethod(my.package.MyParameterClass) :
+     * my.package.MyReturnTypeClass}.
+     * @param className The name of the class from which the method comes,
+     *        including the package.
+     * @param methodName The name of the method itself.
+     * @param params The names of the classes of the parameters of this method,
+     *        including the packages, separated by commas and spaces.
+     * @param returnName The name of the type of class which this method
+     *        returns, including the package.
+     * @return A signature for the specified properties.
+     */
     private static String buildSignature(String className, String methodName, String params, String returnName)
     {
         return className + "." + methodName + "(" + params + ") -> " + returnName;
     }
 
-    // Basically a fixed version of IRewriteCompilationUnit.getRewrittenNode
+    /**
+     * Determines whether or not the specified AST node has pending changes
+     * according to the compilation unit's
+     * {@link IRewriteCompilationUnit#writer() rewriter}.<br>
+     * <br>
+     * This is basically a version of
+     * {@link IRewriteCompilationUnit#getRewrittenNode(ASTNode)} with a few
+     * fixed bugs.
+     * @param node The node to check for pending changes.
+     * @param root The root compilation unit for this node.
+     * @return {@code true} if the specified node has pending changes.
+     */
     @SuppressWarnings("unchecked")
     private static boolean nodeHasChanges(ASTNode node, IRewriteCompilationUnit root)
     {
